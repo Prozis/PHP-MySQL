7c7d92feeb0b154605cd17d3b848da672dc1d59f
Andy Proz <prozius@tut.by>
2019-12-02 17:02:26 +0300

    add file

---
+++ index.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="ru" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="/css/style.css">
+    <title>Main</title>
+  </head>
+  <body>
+    <h3>Список пользователей:</h3>
+      <?php
+      require_once 'config.php';
+          /* Подключение к серверу MySQL */
+      $link = mysqli_connect(SERVER, USER, PASSWORD, DB_NAME);
+
+      if (!$link) {
+         printf("Невозможно подключиться к базе данных. Код ошибки: %s\n", mysqli_connect_error());
+         exit;
+      }
+
+      /* Посылаем запрос серверу */
+      $query = 'SELECT * FROM users';
+      if ($result = mysqli_query($link, $query)) {
+
+        //  print("Вывод содержимого:<br>");
+
+          /* Выборка результатов запроса */
+        //  $db_content = array();
+          echo '<table border="1"> <tr id="title"><td>Номер</td><td>Имя</td><td>Возраст</td><td>E-mail</td></tr>';
+          while( $row = mysqli_fetch_assoc($result) ){
+
+          echo "<tr><td>".$row['id']."</td><td>".$row['name']."</td><td>".$row['age']."</td><td>".$row['email']."</td></tr>";
+
+          }
+          echo "</table>";
+
+          /* Освобождаем используемую память */
+          mysqli_free_result($result);
+      }
+
+      /* Закрываем соединение */
+      mysqli_close($link);
+      //echo "<pre>".print_r($db_content)."</pre>";
+
+      ?>
+
+  <form class="base_add" action="index.php" method="get">
+      <p>
+        <label for="name">Имя</label>
+        <input type="text" name="name" ><br>
+        <label for="age">Возраст</label>
+        <input type="text" name="age" ><br>
+        <label for="email">E-mail</label>
+        <input type="text" name="email" >
+      </p>
+      <button type="submit" name="button">Добавить пользователя</button>
+      <a href="#"></a>
+    </form>
+<?php
+$link = mysqli_connect(SERVER, USER, PASSWORD, DB_NAME);
+
+if (!$link) {
+   printf("Невозможно подключиться к базе данных. Код ошибки: %s\n", mysqli_connect_error());
+   exit;
+}
+
+/* Посылаем запрос серверу */
+$query = 'SELECT * FROM goods';
+if ($result = mysqli_query($link, $query)) {
+  
+    /* Выборка результатов запроса */
+    $db_content = array();
+    while( $row = mysqli_fetch_assoc($result) ){
+      $db_content[] = $row;
+
+    }
+
+    /* Освобождаем используемую память */
+    mysqli_free_result($result);
+}
+
+/* Закрываем соединение */
+mysqli_close($link);
+?>
+    <div class="my_sql">
+
+
+<?php
+
+    /* Подключение к серверу MySQL */
+$link = mysqli_connect(SERVER, USER, PASSWORD, DB_NAME);
+
+if (!$link) {
+   printf("Невозможно подключиться к базе данных. Код ошибки: %s\n", mysqli_connect_error());
+   exit;
+}
+
+/* Посылаем запрос серверу */
+$query = 'SELECT * FROM goods';
+if ($result = mysqli_query($link, $query)) {
+
+    print("Вывод содержимого:<br>");
+
+    /* Выборка результатов запроса */
+    $db_content = array();
+    while( $row = mysqli_fetch_assoc($result) ){
+      $db_content[] = $row;
+
+    }
+
+    /* Освобождаем используемую память */
+    mysqli_free_result($result);
+}
+
+/* Закрываем соединение */
+mysqli_close($link);
+echo print_r($db_content);
+
+?>
+  </div>
+  </body>
+</html>
